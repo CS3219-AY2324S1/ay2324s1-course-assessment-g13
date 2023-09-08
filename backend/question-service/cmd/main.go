ec90da9a4cab891d84967f2072b1ad1d37919a4a
JovonLim <jovonlim@gmail.com>
2023-09-08 23:28:34 +0800

    Setup template

---
+++ backend/question-service/cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	fmt.Println("Starting development server")
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(200, "Hello World")
+	})
+
+	e.Start(":8080")
+}
