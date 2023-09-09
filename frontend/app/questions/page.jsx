2de1f88e3076125c274ca764a28b7067968f4ab2
rexong <rexonghanwei200035@gmail.com>
2023-09-10 00:57:49 +0800

    Add table with pagination

---
+++ frontend/app/questions/page.jsx
@@ -0,0 +1,19 @@
+import { Button } from "@nextui-org/button"
+import QuestionsTable from "./questions-table";
+
+export default function Questions() {
+
+    return (
+        <div className="questions mx-auto max-w-7xl px-6 h-4/5 my-10">
+            <div className="questions-header flex justify-between items-center mb-5">
+                <span className="text-3xl">Question Bank</span>
+                <Button color="primary" variant="ghost" className="text-xl py-6">
+                    Add Question
+                </Button>
+            </div>
+            <div className="table w-full">
+                <QuestionsTable />
+            </div>
+        </div>
+    )
+}
