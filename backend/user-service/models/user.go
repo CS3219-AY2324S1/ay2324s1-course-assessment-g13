6ed6d9205f5f8b990919f7627d8893c50a0bcd17
ThomasHoooo <e0725843@u.nus.edu>
2023-09-09 12:58:19 +0800

    Allow update of username or password

--- backend/user-service/models/user.go
+++ backend/user-service/models/user.go
@@ -12,3 +12,8 @@ type CreateUserRequest struct {
 	Username string `json:"username" validate:"required"`
 	Password string `json:"password" validate:"required"`
 }
+
+type UpdateUserRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
