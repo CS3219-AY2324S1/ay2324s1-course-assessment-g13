983e70880f0d80a3f9efb04d08ffd89c6d8e7651
yeo-yiheng <yeoyiheng0211@gmail.com>
2023-09-10 17:46:33 +0800

    Segregate dockerfiles into prod and staging

--- backend/user-service/config/config.go
+++ backend/user-service/config/config.go
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package config
 
 import (
@@ -35,3 +36,49 @@ func ConnectDb() {
 	DB.AutoMigrate(&model.User{})
 
 }
+=======
+package config
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+	model "user-service/models"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+
+func ConnectDb() {
+	var err error
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal("Unable to load environment variables, with error: ", err)
+		os.Exit(2)
+	}
+
+	dsn := fmt.Sprintf(
+		"host=db user=%s password=%s dbname=%s port=5432 sslmode=disable",
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB"),
+	)
+
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	if err != nil {
+		log.Fatal("Failed to connect to database. \n", err)
+		os.Exit(2)
+	}
+
+	log.Println("running migrations")
+	DB.AutoMigrate(&model.User{})
+
+}
+>>>>>>> ec0251b (Segregate dockerfiles into prod and staging)
--- backend/user-service/main.go
+++ backend/user-service/main.go
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:backend/user-service/cmd/main.go
 package main
 
 import (
@@ -36,3 +37,35 @@ func main() {
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
+=======
+package main
+
+import (
+	"fmt"
+	"user-service/config"
+
+	"user-service/handlers"
+
+	"github.com/gorilla/sessions"
+	"github.com/labstack/echo-contrib/session"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	config.ConnectDb()
+
+	fmt.Println("Starting development server")
+	e := echo.New()
+	e.Use(session.Middleware(sessions.NewCookieStore([]byte("Secret"))))
+
+	e.POST("/users", handlers.CreateUser)
+	e.GET("/users", handlers.GetUsers)
+	e.GET("/users/:id", handlers.GetUser)
+	e.PUT("/users/:id", handlers.UpdateUser)
+	e.DELETE("/users", handlers.DeleteUser)
+
+	e.POST("/login", handlers.Login)
+
+	e.Start(":8080")
+}
+>>>>>>> ec0251b (Segregate dockerfiles into prod and staging):backend/user-service/main.go
