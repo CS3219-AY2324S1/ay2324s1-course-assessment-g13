6618a683659ec536c2aa520e70d7c52f55c6b086
Wu Changjun <wuchangjun98@hotmail.com>
2023-09-16 16:19:29 +0800

    add createNoopStorage

--- frontend/app/redux/store.ts
+++ frontend/app/redux/store.ts
@@ -9,12 +9,28 @@ import {
   persistReducer,
   persistStore,
 } from 'redux-persist';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import questionBankReducer from './slices/questionBankSlice';
+import createWebStorage from 'redux-persist/lib/storage/createWebStorage';
+
+const createNoopStorage = () => {
+  return {
+    getItem() {
+      return Promise.resolve(null);
+    },
+    setItem(_key, value) {
+      return Promise.resolve(value);
+    },
+    removeItem() {
+      return Promise.resolve();
+    },
+  };
+};
+
+const storage = typeof window !== 'undefined' ? createWebStorage('local') : createNoopStorage();
 
 const persistConfig = {
   key: 'root',
-  storage: AsyncStorage,
+  storage,
   whitelist: ['questionBank'],
 };
 
