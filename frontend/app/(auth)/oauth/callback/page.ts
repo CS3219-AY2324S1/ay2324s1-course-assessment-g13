c35eef58110c9899d2a45332442e1eed8a696bec
rexong <rexonghanwei200035@gmail.com>
2023-09-25 18:07:28 +0800

    FE: Fix lint for OAuthCallback

--- frontend/app/(auth)/oauth/callback/page.ts
+++ frontend/app/(auth)/oauth/callback/page.ts
@@ -1,18 +1,15 @@
 "use client";
 import { useSearchParams } from "next/navigation";
 import useAuth from "../../hooks/useAuth";
-import { useCallback, useEffect } from "react";
+import { useEffect } from "react";
 
 export default function OAuthCallback() {
     const param = useSearchParams();
     const code = param.get('code')
     const { handleGithubLoginCallback } = useAuth();
 
-    const memoHandleGithubLoginCallback = useCallback(() => {
-        handleGithubLoginCallback(code);
-      }, [handleGithubLoginCallback, code]);
-
     useEffect(() => {
-        memoHandleGithubLoginCallback();
-    }, [memoHandleGithubLoginCallback])   
+        handleGithubLoginCallback(code);
+        // eslint-disable-next-line
+    }, [])   
 }
