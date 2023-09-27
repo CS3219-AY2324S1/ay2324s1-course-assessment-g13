e3b72442f8ac5de8df103801606d6e396047e603
rexong <rexonghanwei200035@gmail.com>
2023-09-27 22:19:35 +0800

    FE: Bug Fix on middleware

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -2,8 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
     const jwtExist = request.cookies.get('jwt');
-    const url = request.nextUrl.origin
-    const pathname = request.nextUrl.pathname
+    const url = request.nextUrl.origin;
+    const pathname = request.nextUrl.pathname;
 
     const pathIsProtected = protectedPath.indexOf(pathname) !== -1;
 
@@ -12,22 +12,24 @@ export function middleware(request: NextRequest) {
     }
 
     if (!jwtExist && pathIsProtected) {
-        return NextResponse.redirect(url);
+        return NextResponse.redirect(url+'/');
     }
     
-    return NextResponse.next();
 }
-
-export const config = {
-    matcher: "/",
-};
-
-export const protectedPath = [
-    "/questions"
-]
 
 export const unprotectedPath = [
     "/login",
     "/signup",
     "/"
 ]
+
+export const protectedPath = [
+    "/questions",
+    "/profile/info",
+    "/profile/account"
+]
+
+export const config = {
+    matcher: ["/", "/questions", "/profile/info", "/profile/account", "/login", "/signup"]
+};
+
