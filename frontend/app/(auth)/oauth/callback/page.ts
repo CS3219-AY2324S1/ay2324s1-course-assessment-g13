0dfa8ba9ee01191e287cd83cc533ebd6ef3f3fb8
rexong <rexonghanwei200035@gmail.com>
2023-09-30 20:15:37 +0800

    FE: Refactor OAuthCallBack

--- frontend/app/(auth)/oauth/callback/page.ts
+++ frontend/app/(auth)/oauth/callback/page.ts
@@ -1,15 +1,27 @@
 "use client";
-import { useSearchParams } from "next/navigation";
-import useAuth from "../../../hooks/useAuth";
-import { useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { GET } from "../../../libs/axios/axios";
+import { notifyError, notifySuccess } from "../../../components/Notifications";
+import { login } from "../../../libs/redux/slices/userSlice";
+import { useDispatch } from "react-redux";
 
 export default function OAuthCallback() {
     const param = useSearchParams();
-    const code = param.get('code')
-    const { handleGithubLoginCallback } = useAuth();
+    const code = param.get('code');
+    const dispatch = useDispatch();
+    const router = useRouter();
 
-    useEffect(() => {
-        handleGithubLoginCallback(code);
-        // eslint-disable-next-line
-    }, [])   
+    const handleGithubLoginCallback = async (code: string) => {
+        try {
+            const response = await GET(`/auth/login/github?code=${code}`)
+            notifySuccess(response.data.message);
+            dispatch(login(response.data.user));
+            router.push('/questions');
+        } catch (error) {
+            notifyError(error.data.error);
+            router.push("/login");
+        }
+    }
+
+    handleGithubLoginCallback(code); 
 }
