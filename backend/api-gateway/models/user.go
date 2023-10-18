6b2c08b6a66c3884ea606c02a3083d95d207c313
rexong <rexonghanwei200035@gmail.com>
2023-10-18 17:07:19 +0800

    Update User struct

--- backend/api-gateway/models/user.go
+++ backend/api-gateway/models/user.go
@@ -4,10 +4,7 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	UserId   int    `json:"id"`
+	OauthID  int    `json:"oauth_id"`
 	Provider string `json:"provider"`
-	Username string `json:"username"`
-	Email    string `json:"email,omitempty"`
-	Picture  string `json:"picture"`
 	Role     string `json:"role" gorm:"default:'user'"`
 }
